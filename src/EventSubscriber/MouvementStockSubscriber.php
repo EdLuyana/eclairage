523db683219e4c72e64c642b2c3a9d43e1057c48
Ed <edouard.duron@lapiscine.pro>
2025-06-04 14:54:19 +0200

    add CRUD MouvementStock

--- src/EventSubscriber/MouvementStockSubscriber.php
+++ src/EventSubscriber/MouvementStockSubscriber.php
@@ -2,19 +2,43 @@
 
 namespace App\EventSubscriber;
 
-use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use App\Entity\MouvementStock;
+use App\Entity\Stock;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Event\PostPersistEventArgs;
+use Doctrine\Bundle\DoctrineBundle\Attribute\AsEntityListener;
 
-class MouvementStockSubscriber implements EventSubscriberInterface
+#[AsEntityListener(event: 'postPersist', entity: MouvementStock::class)]
+class MouvementStockSubscriber
 {
-    public function onPostPersist($event): void
-    {
-        // ...
-    }
+public function __construct(private EntityManagerInterface $em) {}
 
-    public static function getSubscribedEvents(): array
-    {
-        return [
-            'postPersist' => 'onPostPersist',
-        ];
-    }
+public function postPersist(MouvementStock $mouvement, PostPersistEventArgs $event): void
+{
+$product = $mouvement->getProduct();
+$emplacement = $mouvement->getEmplacement();
+
+// Chercher le stock correspondant
+$stockRepo = $this->em->getRepository(Stock::class);
+$stock = $stockRepo->findOneBy([
+'product' => $product,
+'emplacement' => $emplacement,
+]);
+
+if (!$stock) {
+// S’il n’existe pas, on le crée
+$stock = new Stock();
+$stock->setProduct($product);
+$stock->setEmplacement($emplacement);
+$stock->setQuantity(0);
+}
+
+// Mise à jour de la quantité
+$newQuantity = $stock->getQuantity() + $mouvement->getQuantity();
+$stock->setQuantity($newQuantity);
+$stock->setUpdatedAt(new \DateTimeImmutable());
+
+$this->em->persist($stock);
+$this->em->flush();
+}
 }
---
+++ src/Form/MouvementStockForm.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Emplacement;
+use App\Entity\MouvementStock;
+use App\Entity\Product;
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class MouvementStockForm extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('quantity')
+            ->add('type')
+            ->add('createdAt', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('commentaire')
+            ->add('product', EntityType::class, [
+                'class' => Product::class,
+                'choice_label' => 'id',
+            ])
+            ->add('emplacement', EntityType::class, [
+                'class' => Emplacement::class,
+                'choice_label' => 'id',
+            ])
+            ->add('user', EntityType::class, [
+                'class' => User::class,
+                'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => MouvementStock::class,
+        ]);
+    }
+}
