1358edef8ef990bc8ddf0f2f8b5d128f433f4fea
Ed <edouard.duron@lapiscine.pro>
2025-06-04 12:49:13 +0200

    add composer form and CRUD product for admins

---
+++ src/Form/ProductForm.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Brand;
+use App\Entity\Category;
+use App\Entity\Emplacement;
+use App\Entity\Product;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProductForm extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom')
+            ->add('color')
+            ->add('price')
+            ->add('reference')
+            ->add('category', EntityType::class, [
+                'class' => Category::class,
+                'choice_label' => 'id',
+            ])
+            ->add('place', EntityType::class, [
+                'class' => Emplacement::class,
+                'choice_label' => 'id',
+                'multiple' => true,
+            ])
+            ->add('brand', EntityType::class, [
+                'class' => Brand::class,
+                'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Product::class,
+        ]);
+    }
+}
