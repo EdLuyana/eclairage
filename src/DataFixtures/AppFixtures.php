b0ed3049774dab1722633a8911c213446561c99a
Ed <edouard.duron@lapiscine.pro>
2025-06-04 15:13:51 +0200

    correction test added non-nullable champ

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -20,42 +20,48 @@ public function load(ObjectManager $manager): void
 {
 // Catégories
 $cat1 = new Category();
-$cat1->setName('Ampoules');
+$cat1->setNom('Ampoules');
 $manager->persist($cat1);
 
 $cat2 = new Category();
-$cat2->setName('Lampes');
+$cat2->setNom('Lampes');
 $manager->persist($cat2);
 
 // Marques
 $brand1 = new Brand();
-$brand1->setName('Philips');
+$brand1->setNom('Philips');
 $manager->persist($brand1);
 
 $brand2 = new Brand();
-$brand2->setName('Osram');
+$brand2->setNom('Osram');
 $manager->persist($brand2);
 
 // Emplacements
 $mag1 = new Emplacement();
-$mag1->setName('Magasin Paris');
+$mag1->setNom('Magasin Paris');
 $manager->persist($mag1);
 
 $mag2 = new Emplacement();
-$mag2->setName('Magasin Lyon');
+$mag2->setNom('Magasin Lyon');
 $manager->persist($mag2);
 
 // Produits
 $prod1 = new Product();
-$prod1->setName('Ampoule LED 60W');
+$prod1->setNom('Ampoule LED 60W');
 $prod1->setBrand($brand1);
 $prod1->setCategory($cat1);
+$prod1->setColor('Blanc');
+$prod1->setPrice('29');
+$prod1->setReference('prod1');
 $manager->persist($prod1);
 
 $prod2 = new Product();
-$prod2->setName('Lampe de bureau');
+$prod2->setNom('Lampe de bureau');
 $prod2->setBrand($brand2);
 $prod2->setCategory($cat2);
+$prod2->setColor('Noir');
+$prod2->setPrice('49');
+$prod2->setReference('prod2');
 $manager->persist($prod2);
 
 // Utilisateurs
@@ -76,14 +82,14 @@ $stock1 = new Stock();
 $stock1->setProduct($prod1);
 $stock1->setEmplacement($mag1);
 $stock1->setQuantity(20);
-$stock1->setUpdatedAt(new \DateTimeImmutable());
+$stock1->setUpdatedAt(new \DateTime());
 $manager->persist($stock1);
 
 $stock2 = new Stock();
 $stock2->setProduct($prod2);
 $stock2->setEmplacement($mag2);
 $stock2->setQuantity(10);
-$stock2->setUpdatedAt(new \DateTimeImmutable());
+$stock2->setUpdatedAt(new \DateTime());
 $manager->persist($stock2);
 
 $manager->flush();
